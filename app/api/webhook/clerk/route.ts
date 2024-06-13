4e31df6886d25c452a5b6e715123b28b7a5d3067
Josh Shire <joshshirechildren@gmail.com>
2024-06-13 12:42:37 -0600

    auth

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -95,7 +95,7 @@ export async function POST(req: Request) {
     }
     
     const iden = await GetidByclerk(id)
-    console.log(`the id is ${iden} `)
+    console.log(`the id is ${iden.clerkId} `)
     // const updatedUser = await UpdateUser(id, user);
     
 
